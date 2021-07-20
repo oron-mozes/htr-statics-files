660c22e8a4f4ecae34a202b10379b9d25bd0023b
mozesoron <oronm@wix.com>
2021-07-20 10:45:56 +0300

    export class

--- public/TestImport.js
+++ public/TestImport.js
@@ -4,3 +4,14 @@ import {myFunction} from './TestImportB.js';
 console.log('Module is in the house..')
 myFunction();
 })()
+
+window.__wixWebComponentRender__ = {
+  WixHTMLElement: class {
+
+    render() {
+      console.log('Loaded class!!!!!')
+    }
+  }
+}
+
+window.dispatchEvent('__wixWebComponentRender__.ready');
