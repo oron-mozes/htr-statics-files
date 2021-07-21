eba51b767e414d0916332cfb7aa6ce309c400a66
mozesoron <oronm@wix.com>
2021-07-21 11:51:17 +0300

    reserve

--- roomsApi.js
+++ roomsApi.js
@@ -2,8 +2,18 @@ const express = require('express')
 const router = express.Router()
 const rooms = require('./rooms.json') ;
 
+const reservation = new Map([]);
+
 router.get('/', (req, res) => {
   res.json(rooms);
+})
+router.post('/reserve', (req, res) => {
+  const {metasiteId, memberId, orderId} = req.body;
+  const key = `${metasiteId}-${memberId}`;
+  const orders = reservation.get(key) || [];
+  orders.push[orderId]
+  reservation.set(key, orders)
+  res.json({memberId, orders, success: true});
 })
 
 
