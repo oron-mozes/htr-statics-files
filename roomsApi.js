8a5c33da241aa6b77cd8e6852926d16882d0b7cd
mozesoron <oronm@wix.com>
2021-07-21 16:20:15 +0300

    visitorId

--- roomsApi.js
+++ roomsApi.js
@@ -8,14 +8,12 @@ router.get('/', (req, res) => {
   res.json(rooms);
 })
 router.post('/reserve', (req, res) => {
-  // const {metaSiteId, orderId} = JSON.parse(req.body);
-  console.log(':::req.body', req.body.metaSiteId)
-  // const key = `${metaSiteId}-${1}`;
-  // const orders = reservation.get(key) || [];
-  // orders.push[orderId]
-  // reservation.set(key, orders)
-  // res.json({memberId:1, orders, success: true});
-  res.json({})
+  const {metaSiteId, orderId, visitorId} = req.body;
+  const key = `${metaSiteId}-${visitorId}`;
+  const orders = reservation.get(key) || [];
+  orders.push[orderId]
+  reservation.set(key, orders)
+  res.json({memberId:visitorId, orders, success: true});
 })
 
 
