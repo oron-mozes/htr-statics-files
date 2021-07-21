798874a6e8bc0b3bdcd4e380427b158ffc7d6726
mozesoron <oronm@wix.com>
2021-07-21 17:50:09 +0300

    my-orders

--- roomsApi.js
+++ roomsApi.js
@@ -22,8 +22,6 @@ router.get('/my-orders', (req, res) => {
   const {metaSiteId, visitorId} = req.body;
   const key = `${metaSiteId}-${visitorId}`;
   const orders = reservation.get(key) || [];
-  orders.push[orderId]
-  reservation.set(key, orders)
   res.json({orders });
 })
 
