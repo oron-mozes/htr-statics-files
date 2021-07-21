1ef0ad2ab0340d1f26182a8832c65107207d387b
mozesoron <oronm@wix.com>
2021-07-21 16:41:59 +0300

    body

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const  cors = require('cors');
 const roomsApi = require('./roomsApi.js');
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 
 express()
   .use(cors())
-  .use(bodyParser.urlencoded({ extended: true}))
-  .use(bodyParser.json())
+  // .use(bodyParser.urlencoded({ extended: true}))
+  // .use(bodyParser.json())
   .use(express.static(path.join(__dirname, 'public')))
   .use('/_api/rooms', roomsApi)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- roomsApi.js
+++ roomsApi.js
@@ -15,7 +15,7 @@ router.post('/reserve', (req, res) => {
   const {metaSiteId, orderId, visitorId} = req.body;
   const key = `${metaSiteId}-${visitorId}`;
   const orders = reservation.get(key) || [];
-  console.log('orderId::', orderId, req.body);
+  console.log('orderId::', metaSiteId, orderId, visitorId, req.body);
   orders.push(orderId);
   reservation.set(key, orders);
   res.json({memberId:visitorId, orders, success: true});
