04582bb862ef634c64145149522cf8bdb53314d4
mozesoron <oronm@wix.com>
2021-07-22 10:07:57 +0300

    db connection

---
+++ dbConfig.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require('mongodb')
+
+const limitedConnection = "mongodb+srv://oronmozes:<password>@cluster0.qcw5u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const client = new MongoClient(limitedConnection)
+const dbName = 'htr'
+
+export default async (req, res, next) => {
+  await client.connect()
+  console.log('Connected successfully to server')
+  const db = client.db(dbName)
+  req.DBManager = {
+    db
+  }
+  next();
+}
--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const  cors = require('cors');
 const roomsApi = require('./roomsApi.js');
+const dbConnection = require('./dbConfig.js');
 
 express()
   .use(cors())
+  .use(dbConnection())
   .use(express.static(path.join(__dirname, 'public')))
   .use('/_api/rooms', roomsApi)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
