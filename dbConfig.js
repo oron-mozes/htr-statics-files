c1f2c988816771b18401dcb957c7e05416df60e9
mozesoron <oronm@wix.com>
2021-07-22 10:16:01 +0300

    connection

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb')
 
-const limitedConnection = "mongodb+srv://oronmozes:<password>@cluster0.qcw5u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const limitedConnection = "mongodb+srv://oronmozes:8C2Ehfgy4JbreSe@cluster0.qcw5u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const client = new MongoClient(limitedConnection)
 const dbName = 'htr'
 
