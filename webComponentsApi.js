247fabd3a6f48871ce6b4742b35e2455e998fa6c
mozesoron <oronm@wix.com>
2021-07-23 13:38:12 +0300

    parse

--- webComponentsApi.js
+++ webComponentsApi.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 router.get('/roomsWidget.js', async (req, res) => {
 
   const {instanceId} = req.query;
-  const content = await fs.readFileSync('./jsTemplates/roomsWidget.js');
+  const content = JSON.parse(await fs.readFileSync('./jsTemplates/roomsWidget.js'));
   console.log('::content:: ', content)
   const finalContent = content.replace("{{instanceId}}", instanceId);
   res.setHeader('content-type', 'text/javascript');
