bf7cfaceb50c4d2bce13ebe45da42c55b76ffb6f
mozesoron <oronm@wix.com>
2021-07-23 13:47:05 +0300

    remove parse

--- webComponentsApi.js
+++ webComponentsApi.js
@@ -6,7 +6,7 @@ router.get('/roomsWidget.js', async (req, res) => {
 
   const {instanceId} = req.query;
   const content = await fs.readFileSync('./jsTemplates/roomsWidget.js', 'utf8');
-  console.log('::content:: ', content, JSON.parse(content))
+  console.log('::content:: ', content)
   const finalContent = content.replace("{{instanceId}}", instanceId);
   res.setHeader('content-type', 'text/javascript');
   res.write(finalContent);
