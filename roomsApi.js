6914aa8eccc7cd8363054c8af2b4d796889a8108
mozesoron <oronm@wix.com>
2021-08-19 14:31:47 +0300

    call params from the right place

--- roomsApi.js
+++ roomsApi.js
@@ -43,7 +43,7 @@ router.delete('/my-orders', async (req, res) => {
 })
 
 router.post('/checkout-url', async (req, res) => {
-  const {instanceId, visitorId, metaSiteId} = req.query;
+  const {instanceId, visitorId, metaSiteId} = req.body;
  
   const orderC = req.DBManager.db.collection(ordersCollection);
   const orders = await orderC.find({metaSiteId, visitorId}).toArray();
