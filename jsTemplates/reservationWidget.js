9dbb6a0dc0cfaa69fa3142eee8f36c698426dae3
mozesoron <oronm@wix.com>
2021-08-25 20:46:22 +0300

    reverse order

--- jsTemplates/reservationWidget.js
+++ jsTemplates/reservationWidget.js
@@ -17,7 +17,7 @@ function addInstance() {
 function getCheckoutUrl (id) {
   
   var uri = `{"a11y":true,"storeUrl":${window.location.origin},"isFastFlow":false,"isPickupFlow":false,"cashierPaymentId":"","origin":"productPage","originType":"buyNow","checkoutId":${id}}`;
-  var res = JSON.stringify(encodeURIComponent(uri));
+  var res = encodeURIComponent(JSON.stringify(uri));
   
   return `/checkout?appSectionParams=${res}`
 }
