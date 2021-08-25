8980090e74e7baab16eeff2ed66bcd250d32e87e
mozesoron <oronm@wix.com>
2021-08-25 20:01:59 +0300

    stringify

--- jsTemplates/reservationWidget.js
+++ jsTemplates/reservationWidget.js
@@ -16,7 +16,7 @@ function addInstance() {
 
 function getCheckoutUrl (id) {
   const query= {"a11y":encodeURIComponent(true),"storeUrl":encodeURIComponent(window.location.origin),"isFastFlow":encodeURIComponent(false),"isPickupFlow":encodeURIComponent(false),"cashierPaymentId":encodeURIComponent(""),"origin":("productPage"),"originType":encodeURIComponent("buyNow"),"checkoutId":encodeURIComponent(id)}
-  return `/checkout?appSectionParams=${query}`
+  return `/checkout?appSectionParams=${JSON.stringify(query)}`
 }
 
 function load() {
