8d8ca0bbd29fe3bc3fa3ecdbd5d145d0d3d4a438
mozesoron <oronm@wix.com>
2022-01-05 15:34:37 +0200

    test

--- jsTemplates/reservationWidget.js
+++ jsTemplates/reservationWidget.js
@@ -273,27 +273,17 @@ function load() {
         quantity: order.quantity,
         price: Number(order.roomDetails.price),
       }));
-      // fetch(`${baseUrl}/test-instance`, {
-      //   method: 'post',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify({
-      //     instanceId: userInstance,
-      //     data: {
-      //       locale: 'en',
-      //       amount: items[0].price.toString(),
-      //       appId: '7cbc47b3-cfc6-4d20-a13d-40cd1521378b',
-      //       appInstanceId: userInstance,
-      //       currency: 'EUR',
-      //       msid: wixEmbedsAPI.getHtmlSiteId(),
-      //       visitorId: authorization.aid
-      //     },
-      //   }),
-      // })
-      //   .then((data) => data.json())
-      //   .then((response) => {
-          // console.log(':::instance ::', response);
+      fetch(`/app-makret-payment-service-server/v1/order?orderId=${window.orderIdCreated}`, {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((data) => data.json())
+        .then((response) => {
+          console.log(':::instance ::', response);
+
+        });
           const ifr = document.createElement('iframe');
           ifr.id ="myCheckoutModal";
           ifr.width = '600';
